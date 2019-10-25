bc51f79be0e4868a4dd3849e61b8b8e8d8eaa9a5
okimari <omaatan.0201@icloud.com>
2019-10-25 17:48:27 +0900

    first commit

--- functions.php
+++ functions.php
@@ -26,7 +26,7 @@ function checkSessionId()
     // ログイン失敗時の処理（ログイン画面に移動）
     // ログイン成功時の処理（一覧画面に移動）
     if (!isset($_SESSION['session_id']) || $_SESSION['session_id'] != session_id()) {
-        header('Location: user_login.php');
+        // header('Location: login.php');
     } else {
         session_regenerate_id(true);
         $_SESSION['session_id'] = session_id();
